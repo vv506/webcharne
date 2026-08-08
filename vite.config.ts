f0e8042d7ad5357cf775afbb62f6d7f9d571c901
vv506 <thuyvy151006@gmail.com>
2026-08-08 16:11:57 +0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
   return {
-    base: './',
+    base: '/webcharne/',
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
